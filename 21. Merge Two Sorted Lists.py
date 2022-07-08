56665a77b3d3060f522a07a98128db8f2987c19d
ananan37326 <1705053@ugrad.cse.buet.ac.bd>
2022-07-09 03:31:17 +0600

    Simple comparison

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+
+        return dummy.next
+
+
+# TEST
+list1 = ListNode(1, ListNode(2, ListNode(4)))
+list2 = ListNode(1, ListNode(3, ListNode(4)))
+s = Solution()
+print(s.mergeTwoLists(list1, list2).val)
