3d1dc4cc1473b0fdc2964be09d2be6f7387bde7b
ananan37326 <1705053@ugrad.cse.buet.ac.bd>
2022-11-12 15:06:12 +0600

    Using counter and checking frequency

---
+++ 383.ransom-note.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=383 lang=python3
+#
+# [383] Ransom Note
+#
+
+# @lc code=start
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return collections.Counter(ransomNote) <= collections.Counter(magazine)
+        
+# @lc code=end
+
