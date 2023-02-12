3ec713018e59618054187b0406d0d893310ab3aa
ananan37326 <1705053@ugrad.cse.buet.ac.bd>
2023-02-13 00:53:41 +0600

    Two stacks used

---
+++ 155.min-stack.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=155 lang=python3
+#
+# [155] Min Stack
+#
+
+# @lc code=start
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minstack[-1] if self.minstack else val)
+        self.minstack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
+
