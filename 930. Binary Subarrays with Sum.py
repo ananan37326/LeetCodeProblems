9753e1c04dd8e6c48e31060aa82c299e018d4dc8
Tousif Tanjim Anan <anantousif@gmail.com>
2024-03-16 01:06:16 +0600

    sliding window

---
+++ 930. Binary Subarrays with Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def numLessOrEqual(x):
+            if x < 0:
+                return 0
+            
+            res = 0
+            l, cur = 0, 0
+
+            for r in range(len(nums)):
+                cur += nums[r]
+
+                while cur > x:
+                    cur -= nums[l]
+                    l += 1
+                    
+                
+                res += (r - l + 1)
+            return res
+
+        return numLessOrEqual(goal) - numLessOrEqual(goal-1)
